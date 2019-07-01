98349ffe297be261a608092ce0c7f0139f2c1eaf
charanjattana648 <singhc5@douglascollege.ca>
2019-06-30 22:55:38 -0700

    ProgressActivity class added

---
+++ src/com/csis3275/Entities/ProgressActivity.java
@@ -0,0 +1,24 @@
+package com.csis3275.Entities;
+
+public class ProgressActivity {
+
+	private String date;
+	//from goals table
+	private double weight;
+	//from goals table
+	private String units;
+	//from goals table
+	private double goalWeight;
+	//from goals table
+	//choice of radio button.
+	private int planChosen;
+	private double caloriesConsumed;
+	private double caloriesBurned;
+	private double currentWeight;
+	
+	
+	
+	
+	
+
+}
