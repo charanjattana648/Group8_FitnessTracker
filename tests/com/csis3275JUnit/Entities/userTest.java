8d1516fd2ca77013173d0d4837bfd9bff8d4e250
harpreet768 <kaurh65@student.douglascollege.ca>
2019-07-21 13:34:33 -0700

    unit test for user, userTest.java is added

---
+++ tests/com/csis3275JUnit/Entities/userTest.java
@@ -0,0 +1,48 @@
+package com.csis3275JUnit.Entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.csis3275.Entities.User;
+
+/**
+ * 
+ * @author harpreet Kaur
+ *
+ */
+class userTest {
+	
+	User u;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		u=new User();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void userName() {
+		//fail("Not yet implemented");
+		String expectedFirstName="harpreet";
+		
+		assertEquals(expectedName,u.getUserFirstName() );
+	}
+
+}
