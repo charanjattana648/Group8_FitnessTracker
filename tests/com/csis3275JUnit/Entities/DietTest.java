c805171899184a53e9953b61cd32ec75682757de
charanjattana648 <singhc5@douglascollege.ca>
2019-07-21 01:20:37 -0700

    tests folder added for unit,integration and system testing

---
+++ tests/com/csis3275JUnit/Entities/DietTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.csis3275JUnit.Entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author charan
+ *
+ */
+class DietTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
