7143efb8edc8c4e1a73eb668e5dd13fc4d4b2f0c
charanjattana648 <singhc5@douglascollege.ca>
2019-07-22 12:36:44 -0700

    CaloriesConsumedTest is added

---
+++ tests/com/csis3275JUnit/Entities/CaloriesConsumedTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.csis3275JUnit.Entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.csis3275.Entities.CaloriesConsumed;
+
+/**
+ * @author Charanpreet Singh
+ *
+ */
+class CaloriesConsumedTest {
+	CaloriesConsumed cc;
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		cc=new CaloriesConsumed();
+	}
+
+	
+
+}
