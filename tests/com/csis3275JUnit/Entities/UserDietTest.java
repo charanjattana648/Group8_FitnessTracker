50b26c5c8b21ec2f4d16a78e7ed5135987d42c70
charanjattana648 <singhc5@douglascollege.ca>
2019-07-22 15:46:41 -0700

    4 tests is added to UserDietTest

--- tests/com/csis3275JUnit/Entities/UserDietTest.java
+++ tests/com/csis3275JUnit/Entities/UserDietTest.java
@@ -8,22 +8,53 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.csis3275.Entities.UserDiet;
+
 /**
- * @author charan
+ * @author Charanpreet Singh
  *
  */
 class UserDietTest {
+	UserDiet ud;
 
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@BeforeEach
 	void setUp() throws Exception {
+		ud=new UserDiet();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_id() {
+		int expected=5;
+		ud.setId(expected);
+		assertNotNull(ud.getId());
+		assertEquals(expected, ud.getId());
 	}
+	
+	@Test	
+	void test_userEmail() {
+		String expected="csj648@gmail.com";
+		ud.setUserEmail(expected);
+		assertTrue(expected.equalsIgnoreCase(ud.getUserEmail()));		
+	}
+	
+	@Test
+	void test_dietId() {
+		int expected=5;
+		ud.setDietId(expected);
+		assertEquals(expected, ud.getDietId());
+	}
+	
+	@Test
+	void test_mealType() {
+		String expected="Breakfast";
+		ud.setMealType(expected);
+		assertEquals(expected, ud.getMealType());
+	}
+
+	
+
 
 }
