c8cb11ec0316ca340d4daf49d9381c3039db7072
charanjattana648 <singhc5@douglascollege.ca>
2019-07-22 13:09:19 -0700

    ProgressActivityTest is added

---
+++ tests/com/csis3275JUnit/Entities/ProgressActivityTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.csis3275JUnit.Entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.csis3275.Entities.ProgressActivity;
+
+/**
+ * @author charan
+ *
+ */
+class ProgressActivityTest {
+	ProgressActivity pa;
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		pa=new ProgressActivity();
+	}
+
+
+}
