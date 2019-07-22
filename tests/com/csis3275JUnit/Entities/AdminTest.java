30e8c7310c937051038ad851530b59501a9a5b12
harpreet768 <kaurh65@student.douglascollege.ca>
2019-07-22 13:20:39 -0700

    AdminTest.java class is added

---
+++ tests/com/csis3275JUnit/Entities/AdminTest.java
@@ -0,0 +1,34 @@
+package com.csis3275JUnit.Entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.csis3275.Entities.Admin;
+
+class AdminTest {
+     Admin a;
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		a=new Admin();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+
+
+}
