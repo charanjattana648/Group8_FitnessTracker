14ab07d05f1fe3ce75fd6acdd7733c90b6ec1e19
Gurinder Singh <singhg69@student.douglascollege.ca>
2019-07-22 20:58:42 -0700

    Updating Workouts GUI JTable and Scroll Pane added

--- src/com/csis3275/Boundary/WorkoutsGUI.java
+++ src/com/csis3275/Boundary/WorkoutsGUI.java
@@ -7,10 +7,13 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.border.BevelBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class WorkoutsGUI {
 
 	private JFrame frame;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -47,58 +50,14 @@ public class WorkoutsGUI {
 		JLabel lblWorkouts = new JLabel("Workouts");
 		lblWorkouts.setHorizontalAlignment(SwingConstants.CENTER);
 		lblWorkouts.setFont(new Font("Open Sans", Font.BOLD, 18));
-		lblWorkouts.setBounds(371, 11, 106, 53);
+		lblWorkouts.setBounds(334, 11, 106, 53);
 		frame.getContentPane().add(lblWorkouts);
 		
-		JLabel lblWorkoutName = new JLabel("Workout Name");
-		lblWorkoutName.setBounds(86, 86, 112, 32);
-		frame.getContentPane().add(lblWorkoutName);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(44, 75, 778, 289);
+		frame.getContentPane().add(scrollPane);
 		
-		JLabel lblWorkoutType = new JLabel("Workout Type");
-		lblWorkoutType.setBounds(86, 129, 112, 23);
-		frame.getContentPane().add(lblWorkoutType);
-		
-		JLabel lblCaloriesBurnt = new JLabel("Calories Burnt");
-		lblCaloriesBurnt.setBounds(86, 163, 112, 23);
-		frame.getContentPane().add(lblCaloriesBurnt);
-		
-		JLabel lblTotalTime = new JLabel("Total Time");
-		lblTotalTime.setBounds(86, 195, 112, 23);
-		frame.getContentPane().add(lblTotalTime);
-		
-		JLabel lblWorkoutDescription = new JLabel("Workout Description");
-		lblWorkoutDescription.setBounds(86, 229, 112, 23);
-		frame.getContentPane().add(lblWorkoutDescription);
-		
-		JLabel lblOutWorkoutName = new JLabel("Name");
-		lblOutWorkoutName.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		lblOutWorkoutName.setHorizontalAlignment(SwingConstants.CENTER);
-		lblOutWorkoutName.setBounds(246, 95, 213, 23);
-		frame.getContentPane().add(lblOutWorkoutName);
-		
-		JLabel lblOutWorkoutType = new JLabel("Type");
-		lblOutWorkoutType.setHorizontalAlignment(SwingConstants.CENTER);
-		lblOutWorkoutType.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		lblOutWorkoutType.setBounds(246, 133, 213, 23);
-		frame.getContentPane().add(lblOutWorkoutType);
-		
-		JLabel lblOutWorkoutCalories = new JLabel("Calories");
-		lblOutWorkoutCalories.setHorizontalAlignment(SwingConstants.CENTER);
-		lblOutWorkoutCalories.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		lblOutWorkoutCalories.setBounds(246, 167, 213, 23);
-		frame.getContentPane().add(lblOutWorkoutCalories);
-		
-		JLabel lblOutWorkoutTime = new JLabel("Time");
-		lblOutWorkoutTime.setHorizontalAlignment(SwingConstants.CENTER);
-		lblOutWorkoutTime.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		lblOutWorkoutTime.setBounds(246, 199, 213, 23);
-		frame.getContentPane().add(lblOutWorkoutTime);
-		
-		JLabel lblOutWorkoutDescription = new JLabel("Description");
-		lblOutWorkoutDescription.setHorizontalAlignment(SwingConstants.CENTER);
-		lblOutWorkoutDescription.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		lblOutWorkoutDescription.setBounds(246, 233, 213, 23);
-		frame.getContentPane().add(lblOutWorkoutDescription);
+		table = new JTable();
+		scrollPane.setColumnHeaderView(table);
 	}
-
 }
