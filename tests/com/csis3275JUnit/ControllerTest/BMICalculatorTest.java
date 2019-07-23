b3143412a18a9870e31b1e8f8a0fd5ae39f39bf6
Gurinder Singh <singhg69@student.douglascollege.ca>
2019-07-23 13:43:54 -0700

    Integration Testing in progress

--- tests/com/csis3275JUnit/ControllerTest/BMICalculatorTest.java
+++ tests/com/csis3275JUnit/ControllerTest/BMICalculatorTest.java
@@ -88,5 +88,52 @@ class BMICalculatorTest {
 		System.out.println("" + body.getBmiValues());
 		System.out.println(cBMI.bmiResult());
 	}
+	
+	@Test
+	void testBMIResultsUnderweight() {
+		final double UNDERWEIGHT = 18.5;
+		final double NORMAL = 24.9;
+		final double OVERWEIGHT = 29.9;
+		
+		body = new BodyMeasurements();
+		
+		String result = "";
+		
+		String unit = "Metric";
+		String heightType = "cm";
+		String weightType = "kg";
+		
+		double heightInCm = 155;
+		double weight = 40;
+		
+		
+		body.setHeightCm(heightInCm);
+		body.setUnit(unit);
+		body.setHeightType(heightType);
+		body.setWeightType(weightType);
+		body.setWeight(weight);
+		
+		double bmi = cBMI.calculateBMI(body);
+		body.setBmiValues(bmi);
+		
+		if(bmi < UNDERWEIGHT) {
+			result = "Based on the bmi results you are Underweight." + "\n" + "You should choose Weight Gain plan";
+			assertEquals(result, cBMI.bmiResult());
+		}
+		else if(bmi >= UNDERWEIGHT && bmi < NORMAL) {
+			result = "Based on the bmi results you are under Normal range." + "\n" + "You should choose Stay FIT plan";
+			assertEquals(result, cBMI.bmiResult());
+		}
+		else if(bmi >= NORMAL && bmi < OVERWEIGHT) {
+			result = "Based on the bmi results you are Overweight." + "\n" + "You should choose Weight Loss plan";
+			assertEquals(result, cBMI.bmiResult());
+		}
+		else {
+			result = "Based on the bmi results you are Obese." + "\n" + "You should choose Extreme Weight Loss plan";
+			assertEquals(result, cBMI.bmiResult());
+		}
+		System.out.println("" + body.getBmiValues());
+		System.out.println(cBMI.bmiResult());
+	}
 
 }
