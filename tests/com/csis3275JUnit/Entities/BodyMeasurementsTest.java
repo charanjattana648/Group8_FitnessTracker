54fabf20faddfac169e397e17c6810dbfd533bbf
Gurinder Singh <singhg69@student.douglascollege.ca>
2019-07-23 01:32:14 -0700

    Junit test case for BodyMeasurements added

---
+++ tests/com/csis3275JUnit/Entities/BodyMeasurementsTest.java
@@ -0,0 +1,14 @@
+package com.csis3275JUnit.Entities;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BodyMeasurementsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
