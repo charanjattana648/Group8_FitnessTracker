90a504abae72d5788d923423f82794bf264f5747
Gurinder Singh <singhg69@student.douglascollege.ca>
2019-07-23 13:16:33 -0700

    Integration testing BodyMeasurements and CalculateBMI

--- tests/com/csis3275JUnit/ControllerTest/BMICalculatorTest.java
+++ tests/com/csis3275JUnit/ControllerTest/BMICalculatorTest.java
@@ -4,11 +4,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import com.csis3275.Controller.CalculateBMI;
+import com.csis3275.Entities.BodyMeasurements;
+
 class BMICalculatorTest {
+	
+	BodyMeasurements body = new BodyMeasurements();
+	CalculateBMI cBMI = new CalculateBMI();
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCalculateBMIMetric() {
+		
+		String unit = "Metric";
+		String heightType = "cm";
+		String weightType = "kg";
+		
+		double heightInCm = 168;
+		double weight = 74;
+		
+		body.setHeightCm(heightInCm);
+		body.setUnit(unit);
+		body.setHeightType(heightType);
+		body.setWeightType(weightType);
+		body.setWeight(weight);
+		
+		double heightInMeters = heightInCm / (double) 100;
+		
+		double bmi = weight / Math.pow(heightInMeters, 2);
+		
+		body.setBmiValues(bmi);
+		
+		assertEquals(bmi, body.getBmiValues());
+		
+		
 	}
 
 }
