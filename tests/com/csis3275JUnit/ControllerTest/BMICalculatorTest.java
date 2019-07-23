aa0a365ce22f46148a229c5e8cf15b0a776988de
Gurinder Singh <singhg69@student.douglascollege.ca>
2019-07-23 13:04:36 -0700

    BMICalculator controller JUnit test case added

---
+++ tests/com/csis3275JUnit/ControllerTest/BMICalculatorTest.java
@@ -0,0 +1,14 @@
+package com.csis3275JUnit.ControllerTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BMICalculatorTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
