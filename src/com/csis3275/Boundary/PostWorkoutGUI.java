db2d0db20e1dd357031493813ddaa0c8d0c71753
Gurinder Singh <singhg69@student.douglascollege.ca>
2019-07-22 20:27:22 -0700

    Post Workout GUI added

---
+++ src/com/csis3275/Boundary/PostWorkoutGUI.java
@@ -0,0 +1,43 @@
+package com.csis3275.Boundary;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class PostWorkoutGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					PostWorkoutGUI window = new PostWorkoutGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public PostWorkoutGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
