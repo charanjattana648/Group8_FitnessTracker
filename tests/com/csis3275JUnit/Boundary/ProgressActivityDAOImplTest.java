79b208d77cd7fa938df04540cb25048184c65521
charanjattana648 <singhc5@douglascollege.ca>
2019-07-23 13:06:55 -0700

    3 methods added and tested successfully in ProgressActivityDAOImplTest.java

--- tests/com/csis3275JUnit/Boundary/ProgressActivityDAOImplTest.java
+++ tests/com/csis3275JUnit/Boundary/ProgressActivityDAOImplTest.java
@@ -5,7 +5,9 @@ package com.csis3275JUnit.Boundary;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
@@ -23,13 +25,16 @@ import com.csis3275.Entities.ProgressActivity;
 import com.csis3275.Entities.UserDiet;
 
 /**
- * @author charan
+ * @author Charanpreet Singh
  *
  */
 class ProgressActivityDAOImplTest {
+
+	ProgressActivity pa;
 	DietDAOImpl dI;
 	UserDiet ud;
 	UserDietDAOImpl udI;
+	String userEmail="csis@gmail.com";
 	SessionFactory factory = null;
 	Session sx = null;
 	Transaction tx = null;
@@ -48,8 +53,90 @@ class ProgressActivityDAOImplTest {
 	 */
 	@AfterEach
 	void tearDown() throws Exception {
+		if(sx!=null)
+		 sx.close();
+		factory.close();
+	}
+	
+	public void progress()
+	{
+
+		pa=new ProgressActivity();		
+		Date d=new Date();
+		SimpleDateFormat sdf=new SimpleDateFormat();
+		pa.setDate(sdf.format(d));
+		pa.setUserEmail(userEmail);
+		pa.setCurrentWeight(60.50);
+		pa.setCaloriesBurned(50);
+		pa.setCaloriesConsumed(220);
 	}
 
-	
+	@Test
+	void test_caloriesConsumed() {
+
+		ArrayList<Double> udList=new ArrayList<Double>();
+		try {
+			sx=factory.openSession();
+			tx=sx.beginTransaction();
+			udList=(ArrayList<Double>) sx.getNamedQuery("getCaloriesConsumedQuery").setParameter("uEmail", userEmail).setMaxResults(7).list();			
+			tx.commit();
+			
+		}catch(HibernateException he)
+		{
+			if(tx!=null)
+			{
+				tx.rollback();
+			}
+			System.err.println(he.getMessage());
+		}
+		assertNotNull(userEmail);
+		assertNotNull(udList);
+		assertFalse(udList.isEmpty());
+	}
+
+	@Test
+	void test_SaveProgress() {
+		progress();
+		String key="";
+		try {
+			sx=factory.openSession();
+			tx=sx.beginTransaction();
+			key=(String) sx.save(pa);		
+			tx.commit();
+			
+		}catch(HibernateException he)
+		{
+			if(tx!=null)
+			{
+				tx.rollback();
+			}
+			System.err.println(he.getMessage());
+		}
+		assertNotNull(pa);
+		assertNotNull(key);
+		assertFalse(key.isEmpty());
+	}
+
+	@Test
+	void test_Progress() {
+		ArrayList<ProgressActivity> paList=new ArrayList<ProgressActivity>();
+		try {
+			sx=factory.openSession();
+			tx=sx.beginTransaction();
+			paList=(ArrayList<ProgressActivity>) sx.getNamedQuery("getProgressQuery").setParameter("uEmail", userEmail).list();			
+			tx.commit();
+			
+		}catch(HibernateException he)
+		{
+			if(tx!=null)
+			{
+				tx.rollback();
+			}
+			System.err.println(he.getMessage());
+		}
+		assertNotNull(userEmail);
+		assertNotNull(paList);
+		assertFalse(paList.isEmpty());
+	}
 
 }
