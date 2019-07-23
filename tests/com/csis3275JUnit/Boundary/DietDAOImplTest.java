e96c4dce3762c108df4e1dc056d51e2f22461bb9
charanjattana648 <singhc5@douglascollege.ca>
2019-07-22 18:48:25 -0700

    DietDAOImplTest is added

---
+++ tests/com/csis3275JUnit/Boundary/DietDAOImplTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.csis3275JUnit.Boundary;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.csis3275.Boundary.DietDAOImpl;
+
+/**
+ * @author charan
+ *
+ */
+class DietDAOImplTest {
+	DietDAOImpl dI;
+	StandardServiceRegistry ssr = null;
+	Metadata meta = null;
+	SessionFactory factory = null;
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+		dI=new DietDAOImpl();
+		try {
+			ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+			meta = new MetadataSources(ssr).getMetadataBuilder().build();
+			factory = meta.getSessionFactoryBuilder().build();
+		} catch (Throwable e) {
+			System.err.println("Error : " + e.getMessage());
+		}
+	}
+
+
+	
+
+}
