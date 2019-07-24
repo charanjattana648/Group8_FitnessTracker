3d4427a310bda905aad071c98f9ee7b5526e850c
Gurinder Singh <singhg69@student.douglascollege.ca>
2019-07-23 23:21:53 -0700

    working on system testing for BodyMeasuremenstDAOImpl

--- tests/com/csis3275JUnit/Boundary/BodyMeasurementsDAOImplTest.java
+++ tests/com/csis3275JUnit/Boundary/BodyMeasurementsDAOImplTest.java
@@ -2,28 +2,42 @@ package com.csis3275JUnit.Boundary;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.csis3275.Boundary.BodyMeasurementsDAOImpl;
+import com.csis3275.Entities.BodyMeasurements;
+
 class BodyMeasurementsDAOImplTest {
-
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
-
+	
+	BodyMeasurementsDAOImpl bodyDao;
+	BodyMeasurements body;
+	
+	StandardServiceRegistry ssr = null;
+	Metadata meta = null;
+	SessionFactory factory = null;
+	Session sx = null;
+	Transaction tx = null;
+	int newId;
+	
 	@BeforeEach
 	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
+		body = new BodyMeasurements();
+		bodyDao = new BodyMeasurementsDAOImpl();
+		try {
+			ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+			meta = new MetadataSources(ssr).getMetadataBuilder().build();
+			factory = meta.getSessionFactoryBuilder().build();
+		} catch (Throwable e) {
+			System.err.println("Error : " + e.getMessage());
+		}
 	}
 
 	@Test
