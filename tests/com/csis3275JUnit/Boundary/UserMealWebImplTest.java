1dcfa56ea4b347bc46ac0d23972b0423d62aad6a
charanjattana648 <singhc5@douglascollege.ca>
2019-07-23 19:30:23 -0700

    UserMealWebImplTest.java is added

---
+++ tests/com/csis3275JUnit/Boundary/UserMealWebImplTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.csis3275JUnit.Boundary;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Charanpreet Singh
+ *
+ */
+class UserMealWebImplTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		//String getJsonMealResult(double caloriesReq)
+	}
+
+}
