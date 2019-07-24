cffefdfed2d1ddf903eebfd93bbcc854a871523e
harpreet768 <kaurh65@student.douglascollege.ca>
2019-07-24 02:33:13 -0700

    Session factory is tested in DailyActivityDAOImplTest.java

--- tests/com/csis3275JUnit/Boundary/DailyActivityDAOImplTest.java
+++ tests/com/csis3275JUnit/Boundary/DailyActivityDAOImplTest.java
@@ -2,33 +2,71 @@ package com.csis3275JUnit.Boundary;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+/**
+ * 
+ * @author harpreet kaur
+ *
+ */
 
+import com.csis3275.Boundary.DailyActivityDAOImpl;
+import com.csis3275.Boundary.DietDAOImpl;
+import com.csis3275.Entities.DailyActivity;
 class DailyActivityDAOImplTest {
-
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+    
+	DietDAOImpl dI;
+	DailyActivity d;
+	DailyActivityDAOImpl dImp;
+	SessionFactory factory = null;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		dI=new DietDAOImpl();
+		d=new DailyActivity();
+		dImp=new DailyActivityDAOImpl();
+		factory=dI.getFactory();
+	}
+	
+	@Test
+	void test_Session_factory() {			
+		assertNotNull(dI);
+		assertNotNull(factory, "factory should be null");
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
+	public void addDailyActivity() {
+		d.setId(4);
+		d.setDate("07/24/2019");
+		d.setUserEmail("hkaur@gmail.com");
+		d.setSleepHour(7);
+		d.setSleepMinute(30);
+		d.setWaterGlass(3);
+		d.setExerciseHour(2);
+		d.setExerciseMinute(45);
+		d.setMeditationHour(1);
+		d.setMeditationMinute(23);
+		d.setWalkingHour(2);
+		d.setWalkingMinute(20);
+		d.setExtraCalories(0);
+		d.setExtraProtein(0);
+		d.setExtraFat(0);
+		d.setWorkType("Heavy");
+		d.setWorkHour(3);
+		d.setWorkMinute(45);
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testDailyActivityAdd() {
+		addDailyActivity();
+		int record=dImp.addDailyActivities(d);
+		assertNotNull(dImp);
+		assertNotNull(d);
+		assertNotNull(record);
+		assertNotEquals(0, record);
 	}
 
 }
