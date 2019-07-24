9b9e2285931948af172f8eff43c2689535975590
harpreet768 <kaurh65@student.douglascollege.ca>
2019-07-23 23:44:40 -0700

    session factory is tested successfully in UserDAOImplTest.java

--- tests/com/csis3275JUnit/Boundary/UserDAOImplTest.java
+++ tests/com/csis3275JUnit/Boundary/UserDAOImplTest.java
@@ -2,33 +2,48 @@ package com.csis3275JUnit.Boundary;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.csis3275.Boundary.DietDAOImpl;
+import com.csis3275.Boundary.UserDAOImpl;
+import com.csis3275.Entities.User;
+
+/**
+ * 
+ * @author harpreet kaur
+ *
+ */
 class UserDAOImplTest {
+	DietDAOImpl dI;
+	User u ;
+	UserDAOImpl ud;
+	SessionFactory factory = null;
+	Session sx = null;
+	Transaction tx = null;
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+	
 
 	@BeforeEach
 	void setUp() throws Exception {
+		dI=new DietDAOImpl();
+		u=new User();
+		ud=new UserDAOImpl();
+		factory=dI.getFactory();
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
+	
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void test_Session_factory() {			
+		assertNotNull(dI);
+		assertNotNull(factory, "factory should be null");
 	}
 
 }
