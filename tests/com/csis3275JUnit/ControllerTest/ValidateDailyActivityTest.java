084f066563154c0b16034e8f418c96e297e1c05b
harpreet768 <kaurh65@student.douglascollege.ca>
2019-07-24 18:28:51 -0700

    when all fields are filled according to constraint is tested successfully in ValidateDailyActivityTest.java

--- tests/com/csis3275JUnit/ControllerTest/ValidateDailyActivityTest.java
+++ tests/com/csis3275JUnit/ControllerTest/ValidateDailyActivityTest.java
@@ -8,27 +8,68 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.csis3275.Controller.ValidateDailyActivity;
+import com.csis3275.Entities.DailyActivity;
+import com.csis3275.Entities.User;
+
 class ValidateDailyActivityTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
-	}
+	DailyActivity da;
+	ValidateDailyActivity vda;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		da=new DailyActivity();
+		vda=new ValidateDailyActivity();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 	}
+	
+
+		
+		int expectectsleepHour=7;
+		int expectedsleepMinute=25;
+		int expectedtwaterGlass=6;
+		int expectedexerciseHour=3;
+		int expectedexerciseMinute=12;
+		int expectedmeditationHour=2;
+		int expectedmeditationMinute=24;
+		int expectedwalkingHour=2;
+		int expectedwalkingMinute=34;
+		double expectedextraCalories=240;
+		double expectedextraProtein=150.0;
+		double expectedextraFat=20.0;
+		int expectedworkHour=8;
+		int expectedtxtworkMinute=30;
+		
+	
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void validateInfotest() {
+		
+		
+		
+		DailyActivity da=vda.validate("7", "25", "6", "3", "12", "2", "24", "2","34", "240", "150.0", "20.0", "8", "30");
+		
+		assertAll("Validating Activities Information failed ",()->assertEquals(expectectsleepHour,da.getSleepHour()),
+				()->assertEquals(expectedsleepMinute,da.getSleepMinute()),
+				()->assertEquals(expectedtwaterGlass,da.getWaterGlass()),
+				()->assertEquals(expectedexerciseHour,da.getExerciseHour()),
+				()->assertEquals(expectedexerciseMinute, da.getExerciseMinute()),
+				()->assertEquals(expectedmeditationHour, da.getMeditationHour()),
+				()->assertEquals(expectedmeditationMinute, da.getMeditationMinute()),
+				()->assertEquals(expectedwalkingHour, da.getWalkingHour()),
+				()->assertEquals(expectedwalkingMinute, da.getWalkingMinute()),
+				()->assertEquals(expectedextraCalories, da.getExtraCalories()),
+				()->assertEquals(expectedextraProtein, da.getExtraProtein()),
+				()->assertEquals(expectedextraFat, da.getExtraFat()),
+				()->assertEquals(expectedworkHour, da.getWorkHour()),
+				()->assertEquals(expectedtxtworkMinute, da.getWorkMinute())
+				);
+		
+		
 	}
 
 }
