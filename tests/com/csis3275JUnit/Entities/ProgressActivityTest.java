a4cedfa89560bd31df2d747a1461401d63d19f4e
charanjattana648 <singhc5@douglascollege.ca>
2019-07-24 22:09:33 -0700

    Comments added for test  ProgressActivityTest

--- tests/com/csis3275JUnit/Entities/ProgressActivityTest.java
+++ tests/com/csis3275JUnit/Entities/ProgressActivityTest.java
@@ -12,7 +12,7 @@ import com.csis3275.Entities.ProgressActivity;
 
 /**
  * @author charanpreet Singh
- *
+ *Testing ProgressActivity Object with getters and setters.
  */
 class ProgressActivityTest {
 	ProgressActivity pa;
@@ -24,36 +24,53 @@ class ProgressActivityTest {
 	void setUp() throws Exception {
 		pa=new ProgressActivity();
 	}
-	
+	/**
+	 * testing instance.
+	 */
 	@Test
 	void test_instance() {
 		assertNotNull(pa);
 	}
 
+	/**
+	 * testing date.
+	 */
 	@Test	
 	void test_date() {
 		String expectedDate="22/07/2019";
 		pa.setDate(expectedDate);
 		assertTrue(expectedDate.equalsIgnoreCase(pa.getDate()));		
 	}
+	/**
+	 * testing userEmail.
+	 */
 	@Test	
 	void test_userEmail() {
 		String expected="csj648@gmail.com";
 		pa.setUserEmail(expected);
 		assertTrue(expected.equalsIgnoreCase(pa.getUserEmail()));		
 	}
+	/**
+	 * testing currentWeight.
+	 */
 	@Test
 	void test_currentWeight() {
 		double expected=65;
 		pa.setCurrentWeight(expected);
 		assertEquals(expected, pa.getCurrentWeight());
 	}
+	/**
+	 * testing consumedCalories.
+	 */
 	@Test
 	void test_consumedCalories() {
 		double expected=1440.60;
 		pa.setCaloriesConsumed(expected);
 		assertEquals(expected, pa.getCaloriesConsumed());
 	}
+	/**
+	 * testing consumedBurned.
+	 */
 	@Test
 	void test_consumedBurned() {
 		double expected=1540.90;
