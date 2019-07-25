13070fee329f5ded5a31b1334c0b7604a488f2b8
charanjattana648 <singhc5@douglascollege.ca>
2019-07-24 22:20:09 -0700

    Comments added for CalculateCaloriesNeeded class.

--- src/com/csis3275/Controller/CalculateCaloriesNeeded.java
+++ src/com/csis3275/Controller/CalculateCaloriesNeeded.java
@@ -4,9 +4,21 @@ import java.util.ArrayList;
 
 import com.csis3275.Boundary.BodyMeasurementsDAOImpl;
 import com.csis3275.Entities.BodyMeasurements;
-
+/**
+ * 
+ * @author Charanpreet Singh
+ * CalculateCaloriesNeeded class which provides calculations for calories needed.
+ */
 public class CalculateCaloriesNeeded {
 
+	/**
+	 * calNeeded() method takes following arguments and returns the calorieNeeded to burn or gain.
+	 * @param txtCaldiffTxt is double value,Calorie difference that tells how much calorie should be burned or gained.
+	 * @param txtCalconsumedTxt double value for calorie consumed.
+	 * @param txtCalBurned double value for calorie Burned.
+	 * @param userEmail is a String unique id of user.
+	 * @return calNeeded ,which is double (Calorie Needed to be burnt or gained).
+	 */
 	public double calNeeded(String txtCaldiffTxt,String txtCalconsumedTxt,String txtCalBurned,String userEmail )
 	{
 		BodyMeasurementsDAOImpl bmI=new BodyMeasurementsDAOImpl();
